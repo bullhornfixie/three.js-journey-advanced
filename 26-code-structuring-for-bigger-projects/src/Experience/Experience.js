7f35e3b97c722c068a58752eede9c3efa18d94c4
Jon Dawson <dawson.jon@hotmail.com>
2021-12-07 12:27:13 +0000

    new experience class

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -0,0 +1,11 @@
+export default class Experience 
+{
+    constructor() 
+    {
+      // Global access
+      // This enables you to access class via console
+      window.experience = this 
+      
+      console.log('Here starts the experience')
+    }
+}
