bc8c6e84f98d7c50413b7d722a06134b6e3c2cba
Jon Dawson <dawson.jon@hotmail.com>
2021-12-07 16:58:12 +0000

    resolved orbit controls error

--- 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
@@ -1,12 +1,35 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 export default class Camera
 {
     constructor(experience)
     {
+        // Properties 
         this.experience = experience
         this.sizes = this.experience.sizes
         this.scene = this.experience.scene
-        this.canvas = this.experience.scene
+        this.canvas = this.experience.canvas
 
-        console.log(this)
+        this.setInstance()
+        this.setOrbitControls()
+   
+        // Class and properties 
+        console.log(this) 
+        console.log(this.canvas)
+    }
+
+    // New Camera 
+    setInstance()
+    {
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
+        this.instance.position.set(6, 4, 8)
+        this.scene.add(this.instance)
+    }
+
+    // Orbit Controls
+    setOrbitControls()
+    {
+    this.controls = new OrbitControls(this.instance, this.canvas)
     }
 }
