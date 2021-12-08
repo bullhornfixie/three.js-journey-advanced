5df20ca96ba4cf8f336e5e700cf3025b99a9a888
Jon Dawson <dawson.jon@hotmail.com>
2021-12-08 20:24:02 +0000

    canvas resizing to viewport

--- 26-code-structuring-for-bigger-projects/src/Experience/Renderer.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Renderer.js
@@ -24,7 +24,26 @@ export default class Renderer
           canvas: this.canvas,
           antialias: true
         })
-
-        console.log(this.instance)
+        this.instance.physicallyCorrectLights = true
+        this.instance.outputEncoding = THREE.sRGBEncoding
+        this.instance.toneMapping = THREE.CineonToneMapping
+        this.instance.toneMappingExposure = 1.75
+        this.instance.shadowMap.enabled = true
+        this.instance.shadowMap.type = THREE.PCFSoftShadowMap
+        this.instance.setClearColor('#211d20')
+        this.instance.setSize(this.sizes.width, this.sizes.height)
+        this.instance.setPixelRatio(this.sizes.pixelRatio)
     }
+
+    resize()
+    {
+        this.camera.resize()
+        this.renderer.resize()
+    }
+
+    update()
+    {
+        this.camera.update()
+    }
+    
 }
