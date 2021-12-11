cd70109bfe6b9d9957c419941b0fca5ec15a6a43
Jon Dawson <dawson.jon@hotmail.com>
2021-12-11 18:12:59 +0000

    smooth fade between animations

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Fox.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Fox.js
@@ -43,9 +43,28 @@ export default class Fox
     {
         this.animation = {}
         this.animation.mixer = new THREE.AnimationMixer(this.model)
-        this.animation.action = this.animation.mixer.clipAction(this.resource.animations[0])
 
-        this.animation.action.play()
+        // 3 available actions
+        this.animation.actions = {}
+        this.animation.actions.idle = this.animation.mixer.clipAction(this.resource.animations[0])
+        this.animation.actions.walking = this.animation.mixer.clipAction(this.resource.animations[1])
+        this.animation.actions.running = this.animation.mixer.clipAction(this.resource.animations[2])
+
+        this.animation.actions.current = this.animation.actions.idle
+        this.animation.actions.current.play()
+
+        // Smooth fade between animation with a 1 second pause
+        this.animation.play = (name) =>
+        {
+            const newAction = this.animation.actions[name]
+            const oldAction = this.animation.actions.current
+
+            newAction.reset()
+            newAction.play()
+            newAction.crossFadeFrom(oldAction, 1)
+
+            this.animation.actions.current = newAction
+        }
     }
 
     update()
