8d3ec061f0b6ab1c0a788d986a33999f0aa5585e
Jon Dawson <dawson.jon@hotmail.com>
2021-12-11 18:52:16 +0000

    changing intensity on debug panel

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
@@ -8,9 +8,17 @@ export default class Environment
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
+        this.debug = this.experience.debug
+
+        // Debug
+        if(this.debug.active)
+        {
+            this.debugFolder = this.debug.ui.addFolder('environment')
+        }
 
         this.setSunLight()
         this.setEnvironmentMap()
+
     }
 
     setSunLight()
@@ -34,7 +42,7 @@ export default class Environment
 
         this.scene.environment = this.environmentMap.texture
 
-        this.setEnvironmentMap.updateMaterial = () => 
+        this.environmentMap.updateMaterials = () => 
         {
             this.scene.traverse((child) => 
             {
@@ -43,11 +51,25 @@ export default class Environment
                     child.material.envMap = this.environmentMap.texture
                     child.material.envMapIntensity = this.environmentMap.intensity
                     child.material.needsUpdate = true
+                   
                 }
             })
         }
 
-        this.setEnvironmentMap.updateMaterial()
-    }
+        // Call above function
+        this.environmentMap.updateMaterials()
 
+        // Debug 
+        if(this.debug.active)
+        {
+            this.debugFolder
+                .add(this.environmentMap, 'intensity')
+                .name('envMapIntensity')
+                .min(0)
+                .max(4)
+                .step(0.01)
+                .onChange(this.environmentMap.updateMaterials)
+        }
+
+    }
 }
