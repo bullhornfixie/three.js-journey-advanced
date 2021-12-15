3d209450074612731cfd882d3288771d1e2e0bf9
Jon Dawson <dawson.jon@hotmail.com>
2021-12-15 20:24:26 +0000

    animated flag

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -46,10 +46,16 @@ const material = new THREE.RawShaderMaterial({
     // Properties
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
-    transparent: true,
-    // wireframe: false,
-    // side: THREE.DoubleSide
+    uniforms:
+    {
+        uFrequency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 }
+    }
+
 })
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
@@ -107,6 +113,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Materials 
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -3,5 +3,5 @@ precision mediump float;
 void main()
 // changes the color 
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 2.0); // R, G, B, Alpha
+    gl_FragColor = vec4(0.0, 0.0, 0.5, 1.0); // R, G, B, Alpha
 }
